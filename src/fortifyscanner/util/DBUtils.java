7469cae86ce5dc27f9658d3dc27f93d8c3bf98c5
Umut Askin <umutaskin@gmail.com>
2022-03-12 23:55:21 +0300

    DBUtils added for ignored rules database user and workspace wide

---
+++ src/fortifyscanner/util/DBUtils.java
@@ -0,0 +1,17 @@
+package fortifyscanner.util;
+
+import java.util.List;
+
+import fortifyscanner.model.FortifyIssueDto;
+
+public class DBUtils {
+	
+	public static List<FortifyIssueDto> eliminateIgnoredRulesAtUserScope() {
+		return null;
+	}
+	
+	public static List<FortifyIssueDto> eliminateIgnoredRulesAtWorkspaceScope() {
+		return null;
+	}
+	
+}
